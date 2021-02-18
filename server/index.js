99d39f24fd8873405d864b9083762d60d1507595
Bryant Mercede <bryantme@buffalo.edu>
2021-02-18 15:14:37 -0500

    node project

--- server/index.js
+++ server/index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
 
-const clientPath = path.join(_dirname,'../client');
-const staticPath = path.join(clientPath,'/static');
-const viewsPath =  path.join(clientPath,'/views');
+const clientPath = path.join(_dirname,'../client/');
+const staticPath = path.join(clientPath,'/static/');
+const viewsPath =  path.join(clientPath,'/views/');
 
 const app = express();
 
 app.set('views-engine','ejs');
 app.set('views', viewsPath);
+
+var x = 0;
+
+const counter = function(req, res, next) {
+    x++;
+    console.log(x);
+    next();
+}
+
+app.use(counter);
+
+app.use(express.static(staticPath));
 
 app.get('/', function(req, res){
     res.render('index');
@@ -19,7 +31,6 @@ app.get('/famous', function(req, res){
     res.render('famous');
 });
 
-app.use(express.static(staticPath));
 
 
 app.listen(2000);
